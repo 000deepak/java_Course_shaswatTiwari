4bb5027459df6cb5d041382a5ece0f8d16f39c68
000deepak <90568067+000deepak@users.noreply.github.com>
2024-12-08 16:46:52 +0530

    Update MathsForDSA.java

--- Lecture-011 - Maths for DSA - 1/code/MathsForDSA.java
+++ Lecture-011 - Maths for DSA - 1/code/MathsForDSA.java
@@ -10,10 +10,29 @@ public class MathsForDSA {
     static void decimalToAnyBase(int decimalNum, int base) {
         int resNum = 0;
         int power = 0;
-        while (decimalNum>0){
-            int rem = decimalNum%base;
-            decimalNum /= base;
-            resNum += rem * power(10,power);
+
+        while (decimalNum > 0) {
+            System.out.println(decimalNum);
+            //811 -> 405 -> 202 -> 101 -> 50 & and so on
+
+            int rem = decimalNum % base; // remainder
+            System.out.println(rem);
+            // 1-> 1 -> 0 -> 1 -> 0 & and so on
+
+            decimalNum /= base; // gives quotient
+            System.out.println(decimalNum);
+            //405-> 202 -> 101 -> 50 -> 25 & and so on
+
+            resNum += rem * Math.pow(10, power);
+            System.out.println("sum  " + resNum);
+            // 1-> 11 -> 11 -> 1011 -> 1011  & and so on
+            //explaination : initial resNum = 0, rem = 1, power = 0, & 10^0 ==1
+            // 1        -> 0 + 1 * 10^0
+            // 11       -> 1 + 1 * 10^1
+            // 11       -> 11 + 1 * 10^2
+            // 1011     -> 11 + 1 * 10^3
+            // 1011     -> 1011 + 1 * 10^4
+            
             power++;
         }
         System.out.println("Result is "+ resNum);
